e268ad59ca9769e3b0896d5655dfe4b29795bc85
YAUIO <nbyauio@gmail.com>
2026-01-06 19:29:41 +0100

    Migrations

---
+++ TIN.Data/Migrations/20260106182640_UniqueConstraintForNames.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TIN.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UniqueConstraintForNames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_SpecNames_Language_Name_SpecId",
+                table: "SpecNames",
+                columns: new[] { "Language", "Name", "SpecId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_SpecNames_Language_Name_SpecId",
+                table: "SpecNames");
+        }
+    }
+}
